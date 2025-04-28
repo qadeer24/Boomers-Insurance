ad58d070b7f1844d54e18c37f020738fb7617d5c
Waiz <waiztahseen@gmail.com>
2025-04-28 21:34:59 +0500

    Initial commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,14 +4,12 @@ import './style.css'
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-
-  const [LoginisActive, setLoginIsActive] = useState(false);
-
-  const LoginClick = () => {
-    setLoginIsActive(!LoginisActive);
-  };
-
-
+    
+      const [LoginisActive, setLoginIsActive] = useState(false);
+    
+      const LoginClick = () => {
+        setLoginIsActive(!LoginisActive);
+      };
   return (
     <>
       <div className="header-landing-page" style={{ backgroundColor: "#ED1C27" }}>
@@ -36,7 +34,7 @@ const Home = () => {
       <div className="nav-bar">
         <div className='my-3'><img src="Icons\Logo.svg" alt="" /></div>
         <div className="d-flex nav-bar-signin-opt-Links">
-          <Link className='nav-bar-signin-opt-Link' to={'/'} style={{color:"#ED1C27", textDecoration:"underline"}}>Home</Link>
+          <Link className='nav-bar-signin-opt-Link' to={'/'} style={{ color: "#ED1C27", textDecoration: "underline" }}>Home</Link>
           <Link className='nav-bar-signin-opt-Link' to={'/'}>Find an Agent</Link>
           <Link className='nav-bar-signin-opt-Link' to={'/'}>Events</Link>
           <Link className='nav-bar-signin-opt-Link' to={'/'}>Locations</Link>
@@ -52,6 +50,202 @@ const Home = () => {
           <div className="d-flex">
             <div className='nav-bar-signin-opt'><img src="Icons\user-solid.svg" alt="" />Signin</div>
           </div>
+        </div>
+      </div>
+
+      {/* Hero Section */}
+      <div className="home-hero-section">
+        <div className='text-center'>
+          <h2>Your Medicare journey should be simple and stress-free—Boomers Insurance is here to provide expert advice with a personal touch.</h2>
+          <p>For over 15 years, Boomers Insurance has been a trusted partner for Baby Boomers like you, helping Medicare beneficiaries in 46 states make confident healthcare choices. Our experienced, licensed agents provide clear, unbiased guidance, ensuring you find the right Medicare plan that fits your needs, lifestyle, and budget—so you can enjoy retirement with peace of mind.</p>
+        </div>
+
+        <div className="d-flex" style={{ marginLeft: "33px" }}>
+          <div className="home-hero-section-box">
+            <p className='text-center mt-4 pt-2'><img src="Images\home-image-1.png" alt="" /></p>
+            <h3 className='text-center'>Talk to an agent</h3>
+            <p className='text-center'>Your Agent helps you enroll, compare Medicare options, and choose the coverage that best fits your needs.</p>
+            <div className='d-flex justify-content-center'>
+              <div className='home-hero-section-box-img'></div>
+              <div style={{ color: "#5F5F5F", fontWeight: "600" }}> 1-800-815-1943</div>
+            </div>
+            <p className='home-hero-section-box-p mt-3'>(TTY:711)</p>
+            <p className='home-hero-section-box-p'>Mon-Fri. 9 AM-5 PM PST</p>
+          </div>
+
+          <div className="home-hero-section-box">
+            <p className='text-center mt-4 pt-2'><img src="Images\home-image-2.png" alt="" /></p>
+            <h3 className='text-center'>Shop and Explore online</h3>
+            <p className='text-center mx-4'>See and compare Medicare Advantage plans available near you and apply online.</p>
+            <p style={{ marginLeft: "70px" }}>Enter zip Code</p>
+            <div className="d-flex">
+              <input type="text" placeholder='' className='home-hero-section-box-input' />
+              <button className='home-hero-section-box-button d-flex'>Get Started <img src="Icons\arrow-right-solid.svg" alt="" /></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      {/* Section-1 */}
+      <div className="home-section-1">
+        <h2 className='text-center'>We recommend plans from every single insurance carrier, including</h2>
+
+        <div className="home-section-1-logos">
+          <img src="Images\section-1-logo-1.png" alt="" />
+          <img src="Images\section-1-logo-2.png" alt="" />
+          <img src="Images\section-1-logo-3.png" alt="" />
+          <img src="Images\section-1-logo-4.png" alt="" />
+        </div>
+
+        <h2 className='text-center'>Why Boomers Insurance?</h2>
+        <p className='text-center'>Choosing the right Medicare plan can feel overwhelming, but you don’t have to navigate it alone. At Boomers Insurance, we simplify the process by offering expert guidance, personalized recommendations, and access to top insurance providers. With our dedicated support, you can make informed decisions about your healthcare with confidence.</p>
+
+        <div className="home-section-1-boxes">
+          <div className="home-section-1-box-1">
+            <h5>A Wide Range of Plan Options</h5>
+            <p>We partner with top-rated insurance carriers to offer a variety of Medicare Advantage, Medicare Supplement, and Prescription Drug Plans, ensuring you get the best coverage possible.</p>
+          </div>
+
+          <div className="home-section-1-box-2">
+            <h5>Personalized, One-on-One Support</h5>
+            <p>Every individual’s healthcare needs are unique, and we take the time to understand your specific situation. Our agents will walk you through your options and help you find the plan that best suits you.</p>
+          </div>
+
+          <div className="home-section-1-box-1">
+            <h5>Unbiased & Transparent Guidance</h5>
+            <p>As an independent agency, we work for you—not the insurance companies. Our goal is to provide honest, impartial recommendations so you can make informed decisions about your healthcare.</p>
+          </div>
+
+          <div className="home-section-1-box-2">
+            <h5>No-Cost Consultation</h5>
+            <p>Our services are 100% free to you. There’s no cost, no obligation—just expert advice to help you find the right Medicare plan.</p>
+          </div>
+        </div>
+        <h2 className='text-center pt-5'>Find Your Match, Your Way</h2>
+        <p className='text-center px-5'>Choosing the right health plan should be easy and convenient. Whether you prefer speaking with an advisor, comparing plans online, or scheduling a call, we’re here to help in the way that works best for you.</p>
+
+        <div className="home-sec-1-images-sections">
+          <div className="home-sec-1-images-sec">
+            <img src="Images\section-1-image-1.png" className='home-sec-1-images-sec-img' alt="" />
+            <h4>Speak with an Agent</h4>
+            <p>Get guidance from licensed e-health with Boomers benefit advisors over the phone.</p>
+            <h6>Availability:</h6>
+            <h5>Monday - Friday, 5 AM - 6 PM PT</h5>
+            <div className='d-flex justify-content-center'>
+              <div className='home-hero-section-box-img'></div>
+              <div style={{ color: "#5F5F5F", fontWeight: "600" }}> 1-800-815-1943</div>
+            </div>
+          </div>
+          <div className="home-sec-1-images-sec">
+            <div className='home-sec-1-images-sec-img-2-wrap'><img src="Images\section-1-image-2.png" className='home-sec-1-images-sec-img-2' alt="" /></div>
+            <h4>Explore Plans Online</h4>
+            <p>Discover and explore plans that include your preferred doctors, prescriptions, and benefits.</p>
+            <h6>🔍 See Plans in My Area</h6>
+          </div>
+          <div className="home-sec-1-images-sec">
+            <img src="Images\section-1-image-3.png" className='home-sec-1-images-sec-img' alt="" />
+            <h4>Make an Appointment</h4>
+            <p>Schedule a call with a licensed Boomer Agent advisor at your convenience.</p>
+            <h6>📅 Book Appointment</h6>
+          </div>
+        </div>
+      </div>
+
+      {/* section-2 */}
+      <div className="home-section-2">
+        <h3 className='text-center'>Learn about your Medicare options</h3>
+        <p className='text-center'>Medicare isn’t one-size-fits-all, and finding the right plan depends on your unique healthcare needs. Whether you're looking for additional benefits, lower out-of-pocket costs, or prescription drug coverage, we’re here to help you compare options and choose the plan that works best for you.</p>
+        <div className="home-section-2-boxes">
+          <div className="home-section-2-box">
+            <div className="home-section-2-box-img-1"></div>
+            <h5>Medicare Advantage Plans</h5>
+            <p>These plans offer additional benefits beyond Original Medicare, such as dental, vision, hearing, and prescription drug coverage. Some plans may have premiums as low as $0 per month.</p>
+          </div>
+          <div className="home-section-2-box">
+            <div className="home-section-2-box-img-2"></div>
+            <h5>Medicare Advantage Plans</h5>
+            <p>These plans offer additional benefits beyond Original Medicare, such as dental, vision, hearing, and prescription drug coverage. Some plans may have premiums as low as $0 per month.</p>
+          </div>
+          <div className="home-section-2-box">
+            <div className="home-section-2-box-img-3"></div>
+            <h5>Medicare Advantage Plans</h5>
+            <p>These plans offer additional benefits beyond Original Medicare, such as dental, vision, hearing, and prescription drug coverage. Some plans may have premiums as low as $0 per month.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Section-3 */}
+      <div className="home-section-3">
+        <div className="home-section-3-text">
+          <span>Get in Touch</span>
+          <h3>Get Expert Support from a Licensed Agent in Your Area</h3>
+          <p>What to Expect When You Call:</p>
+          <p>✅ Answer a few simple questions, such as your Medicare number, doctors, and medications.</p>
+          <p>✅ Compare local plans tailored to your needs.</p>
+          <p>✅ Discover discounts and savings opportunities.</p>
+          <p>✅ Get the same plan at the same price as the insurance provider.</p>
+          <span>Speak to an Licensed Agent Today</span>
+          <div className='d-flex'>
+            <div className='home-section-3-box-img'></div>
+            <div style={{ color: "#202020", fontWeight: "700", fontSize: "21px" }}> 1-800-815-1943</div>
+          </div>
+          <div className='d-flex'>
+            <div className='home-section-3-box-img-2'></div>
+            <div style={{ color: "#202020", fontWeight: "700", fontSize: "21px" }}> Monday – Friday | 9 AM – 5 PM PST</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="footer-sec-1">
+          <div>
+            <div className="d-flex" style={{alignItems: "center"}}>
+              <img src="Icons\Logo.svg" alt="" />
+              <h5>BOOMERS INSURANCE</h5>
+            </div>
+            <p>National Producer ID: #139800001-800-815-1943 (TTY: 711)Licensed in 46 States</p>
+          </div>
+          
+          <div style={{display: "grid"}}>
+            <h5>LINKS</h5>
+            <Link style={{color: "white", marginBottom: "0.5rem"}}>Home</Link>
+            <Link style={{color: "white", marginBottom: "0.5rem"}}>Find an Agent</Link>
+            <Link style={{color: "white", marginBottom: "0.5rem"}}>Events</Link>
+            <Link style={{color: "white", marginBottom: "0.5rem"}}>Locations</Link>
+            <Link style={{color: "white", marginBottom: "0.5rem"}}>Medicare FAQs</Link>
+            <Link style={{color: "white", marginBottom: "0.5rem"}}>Enroll Online</Link>
+          </div>
+          
+          <div style={{display: "grid"}}>
+            <h5>HELPFUL LINKS</h5>
+            <Link style={{color: "white", textDecoration: "underline", marginBottom: "0.5rem"}}>SocialSecurity.gov</Link>
+            <Link style={{color: "white", textDecoration: "underline", marginBottom: "0.5rem"}}>Medicare & You —</Link>
+            <Link style={{color: "white", textDecoration: "underline", marginBottom: "0.5rem"}}>Handbook</Link>
+            <Link style={{color: "white", textDecoration: "underline", marginBottom: "0.5rem"}}>Dental & Vision Plans</Link>
+            <Link style={{color: "white", textDecoration: "underline", marginBottom: "0.5rem"}}>Covered California Info</Link>
+            <Link style={{color: "white", textDecoration: "underline", marginBottom: "0.5rem"}}>Commission</Link>
+          </div>
+          
+          <div>
+            <h5>ARE YOU AN AGENT?</h5>
+            <p>Boomers is always looking for licensed agents.</p>
+            <button className='footer-button d-flex'>Apply Here</button>
+          </div>
+        </div>
+
+        <div className="footer-sec-2">
+          <p>This website contains information about and access to insurance plans for Medicare beneficiaries, individuals soon eligible for Medicare and those advising on behalf of Medicare beneficiaries. It is operated by Boomers Insurance Services, a licensed health insurance agency certified to sell Medicare products. Boomers Insurance Services, is not endorsed by the Centers for Medicare & Medicaid Services (CMS), the Department of Health and Human Services (DHHS), or any other government agency.</p>
+          <p>Boomers Insurance Services, is a licensed and certified representative of Medicare Advantage HMO, POS, PPO and PFFS organizations and a stand-alone prescription drug plans with a Medicare contract. Enrollment in any plan depends on contract renewal. This is not a complete list of plans available in your service area. For a complete listing, please contact 1-800-MEDICARE (1-800-633-4227), 24 hours day, 7 days a week or visit www.Medicare.gov. You must continue to pay your Medicare Part B premium. This information is not a complete description of benefits. Contact the plan for more information. Limitations, copayments and restrictions may apply. Benefits, premiums and/or member cost-share may change on January 1 of each year.</p>
+          <p>We do not offer every plan available in your area. Currently, we represent 16 organizations which offer47 products in your area. Please contact Medicare.gov, 1-800-MEDICARE, or your local State Health Insurance Program (SHIP) to get information on all of your options. Plans are insured or covered by a Medicare Advantage (HMO, PPO and PFFS) organization with a Medicare contract and/or a Medicare approved Part D sponsor. Enrollment in the plan depends on the plan’s contract renewal with Medicare.</p>
+          <p>Medicare Supplement plans are not connected with or endorsed by the U.S. Government or the federal Medicare program.</p>
+          <p>The purpose of this communication is the solicitation of insurance. Contact will be made by an insurance agent/producer or insurance company.</p>
+          <p>This information is available for free in other languages. Please contact a licensed Boomers sales agent at 1-800-815-1943 (TTY: 711). Esta información está disponible gratuitamente en otros idiomas. Póngase en contacto con un agente de ventas certificado de Boomers al 1-800-815-1943 (TTY: 711). 本資訊也有其他語言的免費版本可供選擇。請撥1-800-815-1943（聽障專線：711）與持照 Boomers 銷售代理聯絡。Boomers Insurance services complies with applicable Federal civil rights laws and does not discriminate on the basis of race, color,national origin,age, disability, or sex.</p>
+        </div>
+
+        <div className="footer-sec-3">
+          <p className='text-center'>© 2025. Boomers Insurance Services. Privacy Policy MULTIPLAN_13980000website_M_Approved, last update 4/1/2025</p>
+          <p className='text-center'>Created by Stream Design Studio</p>
         </div>
       </div>
     </>
